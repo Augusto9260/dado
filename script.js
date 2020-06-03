d0caa7f2775052ca44afc8228ee059a8611da77a
augusto9260 <augustoj331@gmail.com>
2020-06-03 16:31:25 -0300

    Atualizado

---
+++ script.js
@@ -0,0 +1,47 @@
+function carrega() {
+	let numeros = Math.floor (Math.random() * 6) + 1;
+	let img = document.getElementById('img');
+
+	if (numeros == 1) {
+		img.setAttribute('src', 'um.png');
+	}
+	else if (numeros == 2) {
+		img.setAttribute('src', 'dois.png');
+	}
+	else if (numeros == 3) {
+		img.setAttribute('src', 'tres.png');
+	}
+	else if (numeros == 4) {
+		img.setAttribute('src', 'quatro.png');
+	}
+	else if (numeros == 5) {
+		img.setAttribute('src', 'cinco.png');
+	}
+	else if (numeros == 6) {
+		img.setAttribute('src', 'seis.png');
+	}
+}
+function carrega1() {
+	let numeros = Math.floor (Math.random() * 6) + 1;
+	let img = document.getElementById('img-um');
+
+	if (numeros == 1) {
+		img.setAttribute('src', 'um.png');
+	}
+	else if (numeros == 2) {
+		img.setAttribute('src', 'dois.png');
+	}
+	else if (numeros == 3) {
+		img.setAttribute('src', 'tres.png');
+	}
+	else if (numeros == 4) {
+		img.setAttribute('src', 'quatro.png');
+	}
+	else if (numeros == 5) {
+		img.setAttribute('src', 'cinco.png');
+	}
+	else if (numeros == 6) {
+		img.setAttribute('src', 'seis.png');
+	}
+}
+
